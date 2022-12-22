ab5546bfd5b6bca8ba33f0ba27bd2bb8ef0cb355
sami-a <sami.abdulaal@gmail.com>
2022-12-22 09:56:09 +0300

    fixed change password and finally centered the div

--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const isLogedIn = require('../helper/isLoggedIn')
+const isLoggedIn = require('../helper/isLoggedIn')
 const postCtrl = require("../controllers/posts")
 
 // let methodOverride = require("method-override");
 // router.use(methodOverride('_method'))
 
 // Creating Posts
-router.get("/post/add",isLogedIn, postCtrl.post_create_get)
-router.post("/post/add",isLogedIn, postCtrl.post_create_post)
+router.get("/post/add",isLoggedIn, postCtrl.post_create_get)
+router.post("/post/add",isLoggedIn, postCtrl.post_create_post)
 
 // Reading Posts
 router.get("/post/index", postCtrl.post_index_get)
 router.get("/post/detail/:id", postCtrl.post_details_get)
 
 // Updating Posts
-router.get("/post/edit/:id",isLogedIn, postCtrl.post_edit_get)
-router.post("/post/update",isLogedIn, postCtrl.post_update_put)
+router.get("/post/edit/:id",isLoggedIn, postCtrl.post_edit_get)
+router.post("/post/update",isLoggedIn, postCtrl.post_update_put)
 
 // Deleting Posts
-router.get("/post/delete/:id",isLogedIn, postCtrl.post_delete_get)
+router.get("/post/delete/:id",isLoggedIn, postCtrl.post_delete_get)
 
 // Reading Posts with search
 router.post("/post/search", postCtrl.post_search_post);
--- routes/profile.js
+++ routes/profile.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const isLogedIn = require('../helper/isLoggedIn')
+const isLoggedIn = require('../helper/isLoggedIn')
 const profileCtrl = require('../controllers/profile');
 
 // let methodOverride = require("method-override");
 // router.use(methodOverride('_method'))
 
 //Read Profile
-router.get("/profile/:id",isLogedIn, profileCtrl.profile_details_get);
+router.get("/profile/:id",isLoggedIn, profileCtrl.profile_details_get);
 
 //Edit Profile
-router.get("/profile/edit/:id",isLogedIn, profileCtrl.profile_edit_get);
-router.post("/profile/update",isLogedIn, profileCtrl.profile_update_put);
+router.get("/profile/edit/:id",isLoggedIn, profileCtrl.profile_edit_get);
+router.post("/profile/update",isLoggedIn, profileCtrl.profile_update_put);
 
 //change pass
-router.get("/profile/changepass/:id",isLogedIn, profileCtrl.profile_changepass_get);
-router.get("/profile/update",isLogedIn, profileCtrl.profile_changepass_put);
+router.get("/profile/changepass/:id",isLoggedIn, profileCtrl.profile_changepass_get);
+router.post("/profile/updatepass",isLoggedIn, profileCtrl.profile_changepass_put);
 
 module.exports = router;
